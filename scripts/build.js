871f04a31121212b925830a728e30aeec474fdc6
Nate Goldman <nnmgoldman@gmail.com>
2015-03-29 13:47:07 -0700

    clean & standardize scripts

--- scripts/build.js
+++ scripts/build.js
@@ -1,60 +1,63 @@
-process.chdir(__dirname);
+process.chdir(__dirname)
 
-var fs = require('fs');
-var sass = require('node-sass');
-var CleanCSS = require('clean-css');
-var pkg = require('../package.json');
+var fs = require('fs')
+var sass = require('node-sass')
+var CleanCSS = require('clean-css')
+var pkg = require('../package.json')
 var md = require('markdown-it')('full', {
   html: true,
   linkify: true,
   typographer: true
-});
+})
+var fsOpts = { encoding: 'utf8' }
 
-sass.render({
-  file: '../source/style.scss',
-  success: buildStyle
-});
-
-buildGuide();
+buildGuide()
+buildStyle()
 
 function buildGuide () {
-  var index = fs.readFileSync('../README.md', { encoding: 'utf8' });
-  var guide = fs.readFileSync('../site/guide.md', { encoding: 'utf8' });
-  var header = fs.readFileSync('../site/partials/_header.html', { encoding: 'utf8' });
-  var footer = fs.readFileSync('../site/partials/_footer.html', { encoding: 'utf8' });
+  var header = fs.readFileSync('../site/partials/_header.html', fsOpts)
+  var footer = fs.readFileSync('../site/partials/_footer.html', fsOpts)
+
+  var index = fs.readFileSync('../README.md', fsOpts)
+  var guide = fs.readFileSync('../site/guide.md', fsOpts)
 
   fs.writeFile('../index.html', header + md.render(index) + footer, function (err) {
-    if (err) { throw err; }
-    console.log('built index.html');
-  });
+    if (err) { throw err }
+    console.log('built index.html')
+  })
 
   fs.writeFile('../guide.html', header + md.render(guide) + footer, function (err) {
-    if (err) { throw err; }
-    console.log('built guide.html');
-  });
+    if (err) { throw err }
+    console.log('built guide.html')
+  })
 }
 
-function buildStyle (result) {
-  var normalize = fs.readFileSync('../node_modules/normalize.css/normalize.css', { encoding: 'utf8' });
-  var style = new CleanCSS().minify(normalize + result.css).styles;
-  var banner = '/* ' + pkg.name + ' v' + pkg.version  + ' - ' +
-                getDate() + ' - ' + pkg.homepage + ' */\n';
+function buildStyle () {
+  var result = sass.renderSync({
+    file: '../source/style.scss'
+  })
+
+  var normalize = fs.readFileSync('../node_modules/normalize.css/normalize.css', fsOpts)
+  var style = new CleanCSS().minify(normalize + result.css).styles
+  var banner = '/* ' + pkg.name + ' v' + pkg.version + ' - ' +
+    getDate() + ' - ' + pkg.homepage + ' */\n'
   var imports = '@import url(http://fonts.googleapis.com/css?family=Lora:400,700,400italic);' +
-                '@import url(http://fonts.googleapis.com/css?family=Montserrat:400,700);';
+    '@import url(http://fonts.googleapis.com/css?family=Montserrat:400,700);'
+
   fs.writeFile('../style.css', banner + imports + style, function (err) {
-    if (err) { throw err; }
-    console.log('built style.css');
-  });
+    if (err) throw err
+    console.log('built style.css')
+  })
 }
 
 function getDate () {
-  var d = new Date();
-  var dd = d.getDate();
-  var mm = d.getMonth()+1;
-  var yyyy = d.getFullYear();
+  var d = new Date()
+  var dd = d.getDate()
+  var mm = d.getMonth() + 1
+  var yyyy = d.getFullYear()
 
-  if (dd<10) { dd='0'+dd; }
-  if (mm<10) { mm='0'+mm; }
+  if (dd < 10) dd = '0' + dd
+  if (mm < 10) mm = '0' + mm
 
-  return mm+'/'+dd+'/'+(''+yyyy).substr(2);
+  return mm + '/' + dd + '/' + ('' + yyyy).substr(2)
 }
--- scripts/serve.js
+++ scripts/serve.js
@@ -1,11 +1,10 @@
-var static = require('node-static');
-
-var file = new static.Server('./');
+var server = require('node-static')
+var file = new server.Server('./')
 
 require('http').createServer(function (request, response) {
   request.addListener('end', function () {
-    file.serve(request, response);
-  }).resume();
-}).listen(3000);
+    file.serve(request, response)
+  }).resume()
+}).listen(3000)
 
-console.log('serving files at http://localhost:3000');
+console.log('serving files at http://localhost:3000')
